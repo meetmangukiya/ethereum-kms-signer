7a1317e3a21512c6710f6117d72839ebb8f01d9e
Meet Mangukiya <meet@flamy.dev>
2021-09-26 08:30:30 +0000

    feat: add get_sig_v

--- ethereum_kms_signer/spki.py
+++ ethereum_kms_signer/spki.py
@@ -1,6 +1,7 @@
 from Crypto.Hash import keccak
 from pyasn1.codec.der.decoder import decode as der_decode
 from pyasn1.type import univ, namedtype
+from ecdsa.ecdsa import generator_secp256k1, Signature
 
 
 class SPKIAlgorithmIdentifierRecord(univ.Sequence):
@@ -24,9 +25,8 @@ class ECDSASignatureRecord(univ.Sequence):
     )
 
 
-def der_encoded_public_key_to_eth_address(pubkey):
-    received_record, _ = der_decode(pubkey, asn1Spec=SPKIRecord())
-    hex_string = str(hex(int(received_record["subjectPublicKey"].asBinary(), 2)))
+def public_key_int_to_eth_address(pubkey: int):
+    hex_string = hex(pubkey).replace("0x", "")
     padded_hex_string = hex_string.replace("0x", "").zfill(130)[2:]
 
     k = keccak.new(digest_bits=256)
@@ -34,7 +34,14 @@ def der_encoded_public_key_to_eth_address(pubkey):
     return "0x" + str(bytes.fromhex(k.hexdigest())[-20:].hex()).upper()
 
 
-def get_sig_r_s(signature):
+def der_encoded_public_key_to_eth_address(pubkey: bytes):
+    received_record, _ = der_decode(pubkey, asn1Spec=SPKIRecord())
+    return public_key_int_to_eth_address(
+        int(received_record["subjectPublicKey"].asBinary(), 2)
+    )
+
+
+def get_sig_r_s(signature: bytes):
     received_record, _ = der_decode(signature, asn1Spec=ECDSASignatureRecord())
     r = int(received_record["r"].prettyPrint())
     s = int(received_record["s"].prettyPrint())
@@ -47,3 +54,18 @@ def get_sig_r_s(signature):
         # s is on wrong side of curve, flip it
         s = max_value_on_curve - s
     return r, s
+
+
+def get_sig_v(signature: bytes, r: int, s: int, expected_address: str):
+    ecdsa_signature = Signature(r, s)
+    pks = ecdsa_signature.recover_public_keys(
+        int(signature.hex(), 16), generator_secp256k1
+    )
+
+    for idx, pk in enumerate(pks):
+        x = int(pk.point.to_bytes("uncompressed").hex(), 16)
+        computed_address = public_key_int_to_eth_address(x)
+        if computed_address == expected_address:
+            return 27 + idx
+
+    raise ValueError("Invalid Signature, cannot compute v, addresses do not match!")
--- tests/test_spki.py
+++ tests/test_spki.py
@@ -1,5 +1,9 @@
 from base64 import b64decode
-from ethereum_kms_signer.spki import der_encoded_public_key_to_eth_address, get_sig_r_s
+from ethereum_kms_signer.spki import (
+    der_encoded_public_key_to_eth_address,
+    get_sig_r_s,
+    get_sig_v,
+)
 
 
 def test_der_encoded_public_key_to_eth_address():
@@ -16,4 +20,30 @@ def test_get_sig_r_s():
     assert get_sig_r_s(b64decode(b64sig)) == (
         0x3F25AFDB7ED67094101CD71109261886DB9ABBF1BA20CC53AEC20BA01C2E6BAA,
         0x54F2192BF0769F3DAD0390DE1CA17C95A83F2B567B5796E7FBA7FD166A0061BF,
+    )
+
+
+def test_get_sig_v():
+    assert (
+        get_sig_v(
+            bytes.fromhex(
+                "a1de988600a42c4b4ab089b619297c17d53cffae5d5120d82d8a92d0bb3b78f2"
+            ),
+            0xFA754063B93A288B9A96883FC365EFB9AEE7ECAF632009BAA04FE429E706D50E,
+            0x6A8971B06CD37B3DA4AD04BB1298FDA152A41E5C1104FD5D974D5C0A060A5E62,
+            "0xE94E130546485B928C9C9B9A5E69EB787172952E",
+        )
+        == 28
+    )
+
+    assert (
+        get_sig_v(
+            bytes.fromhex(
+                "a1de988600a42c4b4ab089b619297c17d53cffae5d5120d82d8a92d0bb3b78f2"
+            ),
+            0x904D320777CEAE0232282CBF6DA3809A678541CDEF7F4F3328242641CEECB0DC,
+            0x5B7F7AFE18221049A1E176A89A60B6C10DF8C0E838EDB9B2F11AE1FB50A28271,
+            "0xE94E130546485B928C9C9B9A5E69EB787172952E",
+        )
+        == 27
     )
