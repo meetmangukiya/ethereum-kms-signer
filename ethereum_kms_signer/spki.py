57c1e5eb9ea71cf26acaa1c31614917019d2b163
Meet Mangukiya <meet@flamy.dev>
2021-09-26 04:34:56 +0000

    feat: add get_sig_r_s

--- ethereum_kms_signer/spki.py
+++ ethereum_kms_signer/spki.py
@@ -17,6 +17,13 @@ class SPKIRecord(univ.Sequence):
     )
 
 
+class ECDSASignatureRecord(univ.Sequence):
+    componentType = namedtype.NamedTypes(
+        namedtype.NamedType("r", univ.Integer()),
+        namedtype.NamedType("s", univ.Integer()),
+    )
+
+
 def der_encoded_public_key_to_eth_address(pubkey):
     received_record, _ = der_decode(pubkey, asn1Spec=SPKIRecord())
     hex_string = str(hex(int(received_record["subjectPublicKey"].asBinary(), 2)))
@@ -25,3 +32,18 @@ def der_encoded_public_key_to_eth_address(pubkey):
     k = keccak.new(digest_bits=256)
     k.update(bytes.fromhex(padded_hex_string))
     return "0x" + str(bytes.fromhex(k.hexdigest())[-20:].hex()).upper()
+
+
+def get_sig_r_s(signature):
+    received_record, _ = der_decode(signature, asn1Spec=ECDSASignatureRecord())
+    r = int(received_record["r"].prettyPrint())
+    s = int(received_record["s"].prettyPrint())
+
+    max_value_on_curve = (
+        0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
+    )
+
+    if 2 * s >= max_value_on_curve:
+        # s is on wrong side of curve, flip it
+        s = max_value_on_curve - s
+    return r, s
--- tests/test_spki.py
+++ tests/test_spki.py
@@ -1,5 +1,5 @@
 from base64 import b64decode
-from ethereum_kms_signer.spki import der_encoded_public_key_to_eth_address
+from ethereum_kms_signer.spki import der_encoded_public_key_to_eth_address, get_sig_r_s
 
 
 def test_der_encoded_public_key_to_eth_address():
@@ -8,4 +8,12 @@ def test_der_encoded_public_key_to_eth_address():
     assert (
         der_encoded_public_key_to_eth_address(pubkey)
         == "0x7117AC214FD4ECEE0994ACD4A65D95E2F24AB14A"
+    )
+
+
+def test_get_sig_r_s():
+    b64sig = "MEUCID8lr9t+1nCUEBzXEQkmGIbbmrvxuiDMU67CC6AcLmuqAiEAqw3m1A+JYMJS/G8h416DaRJvsZAz8QlTxCphdmY134I="
+    assert get_sig_r_s(b64decode(b64sig)) == (
+        0x3F25AFDB7ED67094101CD71109261886DB9ABBF1BA20CC53AEC20BA01C2E6BAA,
+        0x54F2192BF0769F3DAD0390DE1CA17C95A83F2B567B5796E7FBA7FD166A0061BF,
     )
